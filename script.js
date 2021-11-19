8f9ff81cfa7e41ba3d4d4695a6894d6c4b66b4e8
Anjali <anjalikuku098@gmail.com>
2021-11-19 22:28:14 +0530

    UI done

--- script.js
+++ script.js
@@ -8,54 +8,16 @@ const addTask = document.querySelector(".add-task");
 const title = document.querySelector(".title");
 const category = document.querySelector(".cat");
 const descrip = document.querySelector(".des");
-const t = document.querySelector(".t");
-const d = document.querySelector(".d");
-const c = document.querySelector(".c");
 const stickers = document.querySelector(".stickers");
 const stick = document.querySelector(".stick");
 
 // -----event Listeners--------
+
 plus.addEventListener("click", openmodal);
 closemodal.addEventListener("click", closemod);
 addTask.addEventListener("click", inputValues);
+document.addEventListener("DOMContentLoaded", displayold);
 
-//-------functions-------
-
-// function enterTask(e) {
-//   e.preventDefault();
-//   const title1 = title.value;
-//   const cat1 = category.value;
-//   const des1 = descrip.value;
-//   e.preventDefault();
-//   const newtask = document.createElement("div");
-//   newtask.classList.add("stick");
-//   newtask.innerHTML = `<div class="tooltip hidden">
-//   <div class="delete"><span> Delete</span><i class="fas fa-trash-alt"></i></div>
-//   <div class="edit"><span>Edit</span><i class="fas fa-edit"></i></div>
-// </div>
-// <div class="stickin">
-//   <div>
-//     <h2 class="t">${title1}</h2>
-//     <p class="d">${des1}</p>
-//   </div>
-//     <i class="fas fa-ellipsis-h c"></i>
-// </div>
-// <div class="bottom">
-//   <i class="fas fa-circle worki"></i>
-//   <i class="fas fa-check-square"></i>
-// </div>`;
-//   stickers.appendChild(newtask);
-//   title.value = "";
-//   category.value = "";
-//   descrip.value = "";
-//   closemod();
-// }
-
-for (let i = 0; i < dots.length; i++) {
-  dots[i].addEventListener("click", function () {
-    tooltip[i].classList.toggle("hidden");
-  });
-}
 // ------------modal and tooltips--------
 
 function openmodal() {
@@ -68,32 +30,89 @@ function closemod() {
   overlay.classList.add("hidden");
 }
 
-//taking user input in an array
-
+//json
+let jsonobj = [];
 function inputValues(e) {
-  let titles = [],
-    categories = [],
-    descriptions = [];
-
   e.preventDefault();
-  let temp1 = title.value;
-  titles.push(temp1);
-  let temp2 = category.value;
-  categories.push(temp2);
-  let temp3 = descrip.value;
-  descriptions.push(temp3);
+  let temp = {
+    title: title.value,
+    category: category.value.toLowerCase(),
+    description: descrip.value,
+    id: Math.floor(Math.random() * 10 + 1),
+    completed: false,
+  };
 
-  jsonobj = [];
-  for (let i = 0; i < titles.length; i++) {
-    let tmp = {
-      title: titles[i],
-      category: categories[i],
-      description: descriptions[i],
-    };
-    jsonobj.push(tmp);
-  }
-
-  console.log(jsonobj);
+  jsonobj = JSON.parse(localStorage.getItem("inputvalues")) || [];
+  jsonobj.push(temp);
+  localStorage.setItem("inputvalues", JSON.stringify(jsonobj));
+  title.value = "";
+  category.value = "";
+  descrip.value = "";
+  closemod();
+  displayNew();
 }
 
-//creating a JSON
+//fresh task
+function displayNew() {
+  let todos = JSON.parse(localStorage.getItem("inputvalues")) || [];
+  let n = todos.length;
+  let title1 = todos[n - 1].title;
+  let cat1 = todos[n - 1].category;
+  let des1 = todos[n - 1].description;
+  let checkbox = todos[n - 1].completed;
+  const newtask = document.createElement("div");
+  newtask.classList.add("stick");
+  newtask.innerHTML = `<div class="tooltip hidden">
+      <div class="delete"><span> Delete</span><i class="fas fa-trash-alt"></i></div>
+      <div class="edit"><span>Edit</span><i class="fas fa-edit"></i></div>
+    </div>
+    <div class="stickin">
+      <div>
+        <h2 class="t">${title1}</h2>
+        <p class="d">${des1}</p>
+      </div>
+        <i class="fas fa-ellipsis-h c"></i>
+    </div>
+    <div class="bottom">
+      <i class="fas fa-circle ${cat1}"></i>
+      <i class="fas fa-check-square ${checkbox}"></i>
+    </div>`;
+  stickers.appendChild(newtask);
+}
+
+//localstorage
+
+function displayold() {
+  let todos = JSON.parse(localStorage.getItem("inputvalues")) || [];
+  todos.forEach(function (todo) {
+    let title1 = todo.title;
+    let cat1 = todo.category;
+    let des1 = todo.description;
+    let checkbox = todo.completed;
+    const newtask = document.createElement("div");
+    newtask.classList.add("stick");
+    newtask.innerHTML = `<div class="tooltip hidden">
+        <div class="delete"><span> Delete</span><i class="fas fa-trash-alt"></i></div>
+        <div class="edit"><span>Edit</span><i class="fas fa-edit"></i></div>
+      </div>
+      <div class="stickin">
+        <div>
+          <h2 class="t">${title1}</h2>
+          <p class="d">${des1}</p>
+        </div>
+          <i class="fas fa-ellipsis-h c"></i>
+      </div>
+      <div class="bottom">
+        <i class="fas fa-circle ${cat1}"></i>
+        <i class="fas fa-check-square ${checkbox}"></i>
+      </div>`;
+    stickers.appendChild(newtask);
+  });
+}
+
+//tooltip
+for (let i = 0; i < dots.length; i++) {
+  dots[i].addEventListener("click", function () {
+    tooltip[i].classList.toggle("hidden");
+  });
+}
